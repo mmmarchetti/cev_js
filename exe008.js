83efe1845fc3dd968b27f4ecfbbc6945033ccdf4
marchetti <mmmarchetti@live.com>
2019-06-10 10:55:20 -0300

    exercises

---
+++ exe008.js
@@ -0,0 +1,6 @@
+var vel = 101;
+if (vel > 100) {
+    console.log(`Excedeu o limite de velocidade! MULTADO!`)    ;
+}
+console.log(`A velocidade do carro é de ${vel}km/h`);
+
---
+++ exe009.js
@@ -0,0 +1,6 @@
+var pais = 'USA';
+if (pais === 'Brasil') {
+    console.log(`Brasileiro!`)
+}else{
+    console.log(`Estrangeiro!`)
+}
