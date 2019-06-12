150def43269852ee2398b505e1450694cb60da61
marchetti <mmmarchetti@live.com>
2019-06-12 14:14:40 -0300

    exercises

--- module_d/exercise002/script.js
+++ module_d/exercise002/script.js
@@ -1,1 +1,26 @@
-alert('Olá!')
+function horario() {
+    var tempo = new Date();
+    var hora = tempo.getHours();
+    var img = document.querySelector("img#imagem");
+    var texto = document.querySelector("div#horario");
+    var horario = document.querySelector("div#txthora");
+
+    horario.innerHTML += `${hora} horas`;
+
+    if (hora > 0 && hora < 12){
+        img.src = 'manha.png';
+        texto.innerHTML += `<br>Bom dia!`;
+        document.body.style.background = '#f1c191';
+
+    }else if (hora >= 12 && hora < 18){
+        img.src = 'tarde.png';
+        texto.innerHTML += `<br>Boa Tarde!`;
+        document.body.style.background = '#b9c3cf';
+
+    }else{
+        img.src = 'noite.png';
+        texto.innerHTML += `<br>Boa Noite!`;
+        document.body.style.background = '#0a1722';
+    }
+
+}
