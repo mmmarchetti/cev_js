3e0ea696f15217590ba0b1fe0f19b860dd68bfe9
marchetti <mmmarchetti@live.com>
2019-06-13 10:42:50 -0300

    exercises

--- module_d/exercise003/script.js
+++ module_d/exercise003/script.js
@@ -1,30 +1,46 @@
 function consultar() {
     var nascimento = document.querySelector("input#nascimento").value;
+    var ano_nascimento = Number(nascimento);
     var hoje = new Date();
-    var ano = hoje.getFullYear();
-    var idade = (ano - Number(nascimento));
+    var ano_atual = hoje.getFullYear();
+    var idade = (ano_atual - ano_nascimento);
     var sexo = document.querySelector('input[name="sexo"]:checked').value;
-    var imagem = document.querySelector("img#foto");
-    var frase = document.querySelector("div#cabecalho");
-    window.alert(idade+ ' ' + ano + ' ' + sexo);
+    var divimg = document.querySelector("div#imagem");
+    var txtdet = document.querySelector("p#txtdet");
+    var erro = 0;
 
-    if (sexo === 'feminino'){
-        frase.innerHTML += ` uma mulher de ${idade} anos`;
+    if (nascimento === ''){
+        window.alert('Ano de Nascimento em Branco!');
+        erro +=1;
+    }else if (ano_nascimento < 0 || idade > 110 || ano_nascimento > ano_atual){
+        window.alert('Ano de Nascimento inválido!');
+        erro +=1;
+    }
+
+    if (sexo === 'feminino' && erro === 0){
+        txtdet.innerHTML = `Detectamos uma mulher de ${idade} anos`;
         if (idade < 16){
-            imagem.src = 'menina.png'
+            divimg.innerHTML = `<img id="foto" src="menina.png" alt="Foto">`;
         }else if (idade < 30){
-            imagem.src = 'garota.png'
+            divimg.innerHTML = `<img id="foto" src="garota.png" alt="Foto">`;
+        }else if (idade < 50) {
+            divimg.innerHTML = `<img id="foto" src="mjovem.png" alt="Foto">`;
         }else{
-            imagem.src = 'mulher.png'
+            divimg.innerHTML = `<img id="foto" src="mulher.png" alt="Foto">`;
         }
-    }else{
-        frase.innerHTML += ` um homem de ${idade} anos`;
+
+    }else if (sexo === 'masculino' && erro === 0){
+        txtdet.innerHTML = `Detectamos uma mulher de ${idade} anos`;
         if (idade < 16){
-            imagem.src = 'menino.png'
+            divimg.innerHTML = `<img id="foto" src="menino.png" alt="Foto">`;
         }else if (idade < 30){
-            imagem.src = 'garoto.png'
+            divimg.innerHTML = `<img id="foto" src="garoto.png" alt="Foto">`;
+        }else if (idade < 50){
+            divimg.innerHTML = `<img id="foto" src="hjovem.png" alt="Foto">`;
         }else{
-            imagem.src = 'homem.png'
+            divimg.innerHTML = `<img id="foto" src="homem.png" alt="Foto">`;
         }
     }
+    divimg.style.textAlign = 'center';
+    txtdet.style.textAlign = 'center';
 }
