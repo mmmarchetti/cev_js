4455ba7edd21afe1fe8035d0ec4cbd3b8c97aca6
marchetti <mmmarchetti@live.com>
2019-06-17 14:25:59 -0300

    exercises

---
+++ module_e/exercises05/script.js
@@ -0,0 +1,27 @@
+function contar() {
+    var inicio = Number(document.querySelector("input#inicio").value);
+    var fim = Number(document.querySelector("input#fim").value);
+    var passo = Number(document.querySelector("input#passo").value);
+    var exibe = document.querySelector("p#contagem");
+
+    if (inicio == '' || fim == '' || passo == ''){
+        exibe.innerHTML = `Impossível contar!`
+    }else{
+        exibe.innerHTML = '';
+        if (inicio < fim){
+            for (inicio; inicio<=fim; inicio+=passo){
+                exibe.innerHTML += `${inicio}`;
+                exibe.innerHTML += ` ➡️`
+            }
+        }else{
+            passo *= -1;
+            for (inicio; inicio>=fim; inicio+=passo){
+                exibe.innerHTML += `${inicio}`;
+                exibe.innerHTML += ` ➡️`
+            }
+
+        }
+        exibe.innerHTML += ` 🏁`
+    }
+
+}
