16385669dbd05f1c60a02f6af108203d1c2e6f13
marchetti <mmmarchetti@live.com>
2019-06-24 19:58:18 -0300

    exercises

--- module_f/exercise07/script.js
+++ module_f/exercise07/script.js
@@ -13,10 +13,12 @@ function adicionar() {
     let txtnumero = document.querySelector("input#txtnumero");
     let num = Number(txtnumero.value);
     let campo_numeros = document.querySelector("select#numeros");
+    let analise = document.querySelector("div#resultado");
 
     if (num < 0 || num > 100){
         window.alert('Digite um número entre 1 e 100!')
     }else{
+        analise.innerHTML = `<p></p>`;
         if (num_list.length === 0){
             num_list.push(num);
             campo_numeros.innerHTML = `<option> Valor ${num} adicionado.</option>`;
@@ -30,17 +32,21 @@ function adicionar() {
 }
 
 function finalizar() {
-    let soma = 0;
+    if (num_list.length === 0){
+        window.alert('Adicione um número!')
+    }else{
+        let soma = 0;
 
-    for (let i=0; i<num_list.length; i++){
-        soma += num_list[i]
+        for (let i=0; i<num_list.length; i++){
+            soma += num_list[i]
+        }
+        let media = soma / num_list.length;
+
+        let analise = document.querySelector("div#resultado");
+        analise.innerHTML += `<p>Ao todo temos ${num_list.length} números cadastrados.</p>`;
+        analise.innerHTML += `<p>O maior número informado foi ${Math.max.apply(null, num_list)}.</p>`;
+        analise.innerHTML += `<p>O menor número digitado foi ${Math.min.apply(null, num_list)}</p>`;
+        analise.innerHTML += `<p>A soma dos números digitado é igual a ${soma}</p>`;
+        analise.innerHTML += `<p>A média dos números digitados é igual a ${media}</p>`
     }
-    let media = soma / num_list.length;
-
-    let analise = document.querySelector("div#resultado");
-    analise.innerHTML += `<p>Ao todo temos ${num_list.length} números cadastrados.</p>`;
-    analise.innerHTML += `<p>O maior número informado foi ${Math.max.apply(null, num_list)}.</p>`;
-    analise.innerHTML += `<p>O menor número digitado foi ${Math.min.apply(null, num_list)}</p>`;
-    analise.innerHTML += `<p>A soma dos números digitado é igual a ${soma}</p>`;
-    analise.innerHTML += `<p>A média dos números digitados é igual a ${media}</p>`
 }
